ac85cb308191b536228ab4687db52dcdbd1166c2
Xopar <xoparr@gmail.com>
2021-04-26 17:21:48 -0500

    Updates to the rated async download script. We now can accomodate patreon vanity URLs and the new time submission form

--- download_async.py
+++ download_async.py
@@ -5,9 +5,13 @@ import requests
 import datetime as dt
 
 
+patreon_conversion = {
+    "emosoda": "07QXz83Ke6WeZDjr"
+}
+
+
 class RaceEntrant():
     def __init__(self, sheet_row):
-        sheet_row = sheet_row.split(',')
         self.timestamp1 = sheet_row[0]
         self.display_name = sheet_row[1].split('#')[0].strip()
         self.rturl = sheet_row[3].strip()
@@ -17,24 +21,34 @@ class RaceEntrant():
         self.placement = None
 
     def include_responses(self, resp):
+        # Find the row in responses that matches the entrant
         thisrow = None
         for row in resp:
-            if row[9].strip() == self.rturl:
+            if row[2].strip() == self.rturl:
                 thisrow = row
                 break
 
+        # If a reponse was found, match things
         if thisrow is not None:
             self.timestamp2 = thisrow[0]
-            ftime = thisrow[6].strip().split(':')
-            if len(ftime) == 1: # This is the case where they write something like `forfeit` into the time field
-                self.finish_time = None
-            elif len(ftime) == 2: # If time is under an hour
-                self.finish_time = dt.timedelta(minutes=float(ftime[0]), seconds=round(float(ftime[1])))
-            else:
-                self.finish_time = dt.timedelta(hours=float(ftime[0]), minutes=float(ftime[1]), seconds=round(float(ftime[2])))
+            ftime = thisrow[4].strip()
+            # Account for forfeits that fill out the form
+            if ftime in ['0:00:00', '69:59:59']:
+                return
+            self.finish_time = dt.timedelta(
+                hours=float(ftime.split(':')[0]),
+                minutes=float(ftime.split(':')[1]),
+                seconds=round(float(ftime.split(':')[2])))
 
 
     def build_output_row(self):
+        # Replace patreon vanity URLs
+        userid = self.rturl.split("https://racetime.gg/user/")[1]
+        if userid in patreon_conversion:
+            userid = patreon_conversion[userid]
+        self.rturl = "https://racetime.gg/user/" + userid
+
+        # Write the line for output
         if self.placement is None:
             output = [self.rturl, 'null', self.display_name, 'dnf', 'null']
         else:
@@ -45,6 +59,13 @@ class RaceEntrant():
             output = [self.rturl, self.placement, self.display_name, 'done', prettytime]
 
         return ','.join([str(x) for x in output]) + '\n'
+
+
+def download_asyncs(sheet_list):
+    for sheet in sheet_list:
+        response = requests.get(f"https://docs.google.com/spreadsheets/d/{sheet['doc_id']}/export?format=csv&gid={sheet['tab_id']}")
+        with open(f"google_sheets/{sheet['name']}.csv", 'w') as fp:
+            fp.write(response.text)
 
 
 def main():
@@ -58,17 +79,14 @@ def main():
     # Download the google sheets
     sheet_list = [
         {"name": "requests", "doc_id": "1RGPpMSdsQZtmROEqIsrN_5Nt8Z0RfKZ2EeW-DFj9uus", "tab_id": "133854036"},
-        {"name": "responses", "doc_id": "1rqBlRU3GayPKWjja76A_7bavWdTveVSEjw-JK5o2cQY", "tab_id": "623017156"}
+        {"name": "responses", "doc_id": "1D-QGG-reSHuVzSQP6tzEW_s-TjkNgouIF7U80mNjoJo", "tab_id": "1877272115"}
     ]
-    for sheet in sheet_list:
-        response = requests.get(f"https://docs.google.com/spreadsheets/d/{sheet['doc_id']}/export?format=csv&gid={sheet['tab_id']}")
-        with open(f"google_sheets/{sheet['name']}.csv", 'w') as fp:
-            fp.write(response.text)
+    download_asyncs(sheet_list)
 
     # Change this to keep the files in memory rather than save and reopen
     with open('google_sheets/requests.csv', 'r') as fin:
         req = fin.readlines()[1:]
-        req = [line.strip() for line in req]
+        req = [line.strip().split(',') for line in req]
     with open('google_sheets/responses.csv', 'r') as fin:
         resp = fin.readlines()[1:]
         resp = [line.strip().split(',') for line in resp]
@@ -85,10 +103,11 @@ def main():
             entrants[i].placement = i+1
 
     # Write output file
+    today = dt.date.today()
     with open(f"other_races/rated_async_{asyn_number}.txt", 'w') as fp:
         fp.write(f"Rated Async {asyn_number}\n")
         fp.write(f"rated_async_{asyn_number}\n")
-        fp.write("2020-<MM>-<DD>T04:00:00Z\n")
+        fp.write(f"2021-{today.month:02d}-{today.day:02d}T04:00:00Z\n")
         for entr in entrants:
             fp.write(entr.build_output_row())
         
